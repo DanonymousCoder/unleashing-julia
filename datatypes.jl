aba1b3b3c3568a1b3ca2fe69aee93a090ef8a13d
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-01-29 19:20:59 +0000

    add

---
+++ datatypes.jl
@@ -0,0 +1,17 @@
+x = 10
+y = 2.71828
+rational = 5 // 2
+println(5//2 == 2.5)
+
+total = 12 + 8  # equals to 20
+product = 10 * 2  # equals to 20
+println("The total is ", total)
+println("The product is ", product)
+
+greeting = "Hello, Julia!"
+println(greeting)
+letter_s = "s️❤️"
+println(letter_s)
+
+❤️ = "Julia is love!"
+println(❤️)  # Outputs: Julia is love!
