97da19165c32e50759f3096d58e9fd7916554918
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2025-02-04 23:03:13 +0000

    minor changes

--- notebook.jl
+++ notebook.jl
@@ -2,3 +2,10 @@ println("Hello, World!")
 
 hello(x) = println("Hello, $(x)!")
 hello("Stackie")
+
+begin
+	using DataFrames
+	using CSV
+end
+
+df = CSV.read("WHO-COVID-19-global-data.csv", DataFrame)
